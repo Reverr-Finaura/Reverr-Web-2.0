fd37540210eb1e42131ca7a13a144996e069004d
Aditya <akaditya394@gmail.com>
2022-07-18 14:40:43 +0530

    public folder used in server

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var path = require("path");
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
