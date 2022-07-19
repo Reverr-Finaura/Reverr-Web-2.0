d1723912860eed6e788d737f02449667b26536e4
Aditya <akaditya394@gmail.com>
2022-07-19 11:21:20 +0530

    get route added for contact page

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ var express = require("express");
 var app = express();
 var path = require("path");
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
   res.render("home");
+});
+
+app.get("/contactus", function (req, res) {
+  res.render("contact");
 });
 
 app.listen(3000, (req, res) => {
