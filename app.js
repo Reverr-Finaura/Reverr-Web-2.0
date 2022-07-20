6145adf487c8b029fb1fe2d9588f23507da90271
Aditya <akaditya394@gmail.com>
2022-07-20 23:52:03 +0530

    community page comepleted

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ app.get("/knowledge", function (req, res) {
   res.render("knowledge");
 });
 
+app.get("/community", function (req, res) {
+  res.render("community");
+});
+
 app.listen(3000, (req, res) => {
   console.log("Server up and running at port 3000");
 });
