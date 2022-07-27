f2575d39ab675050d05629033fb5188aa5f6324d
Nikita <nikitakubavat33@gmail.com>
2022-07-27 21:36:14 +0530

    mentorform done

---
+++ config.js
@@ -0,0 +1,18 @@
+const {
+  initializeApp,
+  applicationDefault,
+  cert,
+} = require("firebase-admin/app");
+const { getFirestore, FieldValue } = require("firebase-admin/firestore");
+const serviceAccount = require("./reverr-25fb3-firebase-adminsdk-g8tph-a9d7f58699.json");
+
+initializeApp({
+  credential: cert(serviceAccount),
+});
+
+const db = getFirestore();
+const Mentors = db.collection("Mentors");
+const Message = db.collection("Message");
+const Funding = db.collection("Funding");
+const Newsletter = db.collection("Newsletter");
+module.exports = { Mentors, Funding, Newsletter };
