d4fede0bfeea21bf76e478bdd23730b6aaf02def
Aditya <akaditya394@gmail.com>
2022-07-28 14:42:13 +0530

    funding form post method added

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var express = require("express");
 var app = express();
 var path = require("path");
+const cors = require("cors");
+const bodyparser = require("body-parser");
 const { Mentors, Funding, Newsletter, Message } = require("./config");
 var SibApiV3Sdk = require("sib-api-v3-sdk");
 
+app.use(cors());
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
 app.use(express.static(__dirname + "/public"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -107,8 +112,22 @@ app.get("/test", function (req, res) {
   res.render("test");
 });
 
+app.get("/newsletter", function (req, res) {
+  res.render("newsletter");
+});
+
 app.get("/mentorform", (req, res) => {
   res.render("mentorform", { successful: false });
+});
+
+app.post("/newsletter", async (req, res) => {
+  const newsletter = req.body;
+  try {
+    await Newsletter.add(newsletter);
+    return res.redirect("/");
+  } catch (err) {
+    return res.json({ err });
+  }
 });
 
 app.post("/mentor", async (req, res) => {
@@ -175,6 +194,60 @@ app.post("/mentor", async (req, res) => {
     return res.render("mentorform", { successful: true });
   } catch (err) {
     return res.json({ err });
+  }
+});
+
+app.post("/funding", async (req, res) => {
+  const funding = req.body;
+  console.log(funding);
+  try {
+    await Funding.add(funding);
+
+    const user = {
+      email: req.body.Email,
+      subject: "Applied for Funding",
+      text: `<h1>You have successfully Applied for funding!</h1>
+          <p>Hi ${req.body.name}, We have received your request and would like to thank you for writing to us. </p>
+          <p>We will contact you shortly.</p>
+          <br>
+          <p>Regards</p>
+          <p>Team Reverr</p>`,
+    };
+
+    Email(user);
+
+    const user3 = {
+      email: "bdreverr@gmail.com",
+      subject: "Funding Signup Request",
+      text: `<h1>${req.body.name} has applied for funding!</h1>
+          <p>name:${req.body.name}</p>
+          <p>year_est:${req.body.year_est}</p>
+          <p>website:${req.body.website}</p>
+          <p>Businessmodel:${req.body.Businessmodel}</p>
+          <p>country:${req.body.country}</p>
+          <p>state:${req.body.state}</p>
+          <p>industry:${req.body.industry}</p>
+          <p>sector:${req.body.sector}</p>
+          <p>stage:${req.body.stage}</p>
+          <p>pitchdeck:${req.body.pitchdeck}</p>
+          <p>ElevatorPitch:${req.body.ElevatorPitch}</p>
+          <p>demovideo:${req.body.demovideo}</p>
+          <p>Entitydetail:${req.body.Entitydetail}</p>
+          <p>teamsize:${req.body.teamsize}</p>
+          <p>foundername:${req.body.foundername}</p>
+          <p>Title:${req.body.Title}</p>
+          <p>linkedin:${req.body.linkedin}</p>
+          <p>contactnumber:${req.body.contactnumber}</p>
+          <p>Email:${req.body.Email}</p>
+          <br>
+          <p>Regards</p>
+          <p>Team Reverr</p>`,
+    };
+
+    Email(user3);
+    return res.redirect("/");
+  } catch (err) {
+    return res.json(err);
   }
 });
 
